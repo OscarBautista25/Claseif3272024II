54bc9b96e8bbf3be6840d522ff784ec3ab89dd93
Oscar Bautista <ingenierobautistaoscar@gmail.com>
2024-09-05 08:20:33 -0500

    ejercicio con java

---
+++ MainScanner.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MainScanner {
+    public static void main(String[] args) {
+        Scanner consola = new Scanner(System.in);
+
+        System.out.println("Ingrese el salario sin ajuste: ");
+        float salarioInicial = consola.nextFloat();
+        System.out.println("Ingrese el porcentaje de aumento(por ejemplo 0.1 para 10%): ");
+        float porcentajeAumento = consola.nextFloat();
+        EjercicioIf pruebaScanner = new EjercicioIf(salarioInicial, porcentajeAumento, 0);
+        float salarioAjustado = pruebaScanner.calcularSalario();
+        System.out.println("El salario ajustado es: " +salarioAjustado);
+        consola.close();
+        
+
+    }
+}
